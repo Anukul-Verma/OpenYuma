0628bbbbccfad8912048d70b4f483b181ea9912e
root <root@verma>
2018-11-27 15:43:31 -0800

    writable-running support enable

--- install.sh
+++ install.sh
@@ -11,11 +11,13 @@ cp dest-dir/startup-cfg.xml /root/.yuma/
 
 killall -15 netconfd
 rm -rf /tmp/ncxserver.sock
-netconfd --superuser=root &
+#netconfd --superuser=root &
+netconfd &
 
 echo "delete /nacm" > /tmp/a.txt
 echo "commit" >> /tmp/a.txt
 echo "save" >> /tmp/a.txt
 echo "quit" >> /tmp/a.txt
 #ip netns exec zebosfib1 bash
-yangcli server=0 user=root password=sp run-script=/tmp/a.txt
+#yangcli server=0 user=root password=sp run-script=/tmp/a.txt
+yangcli server=0 user=ocnos password=ocnos run-script=/tmp/a.txt
